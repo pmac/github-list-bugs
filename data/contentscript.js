1f4f8197e21e9edeb6e002b2c4dac6a47a1a404b
Paul McLanahan <paul@mclanahan.net>
2013-04-29 15:52:16 -0400

    Move 'open all' link to same line as bug lings.
    Rework JS a bit more.

--- data/contentscript.js
+++ data/contentscript.js
@@ -25,22 +25,22 @@ function getBugLinks(){
     let bugIds = getBugIds();
 
     return bugIds.map(function(k){
-        return '<a href="' + bugURL + k + '" target="_blank" class="bugzilla-link">' + k + '</a>';
+        return '<a href="' + bugURL + k + '" target="_blank" class="bugzilla_link">' + k + '</a>';
     });
 }
 
 
 var $bugsList = $([
-        '<p class="explain" id="bugzilla-links">Bugs in commits: ',
+        '<p class="explain">Bugs in commits ',
+        '(<a href="#" id="open_all_bugzilla_links">open all</a>): ',
         getBugLinks().join(', '),
         '</p>'
     ].join('')).insertBefore('#commits_bucket .boxed-group:first');
 
-$('<p class="explain"><a href="#">Open all in tabs</a></p>')
-    .insertAfter($bugsList)
-    .find('a').on('click', function(e){
-        e.preventDefault();
-        $bugsList.find('a').each(function(){
-            window.open(this.href);
-        });
+
+$('#open_all_bugzilla_links').on('click', function(e){
+    e.preventDefault();
+    $bugsList.find('a.bugzilla_link').each(function(){
+        window.open(this.href);
     });
+});
