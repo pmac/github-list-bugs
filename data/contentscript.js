bafaa4ae1d3ab28350b26703473b1674e09615fd
Paul McLanahan <paul@mclanahan.net>
2013-04-29 11:04:56 -0400

    Clean up some JS.

--- data/contentscript.js
+++ data/contentscript.js
@@ -5,39 +5,42 @@ var bugRe = /\bbug (\d+)\b/i;
 var bugURL = 'https://bugzilla.mozilla.org/show_bug.cgi?id=';
 
 
-function getBugLinks() {
-    var bugIds = [];
+function getBugIds() {
+    let bugIds = [];
     $('a.message, div.commit-desc pre').each(function(){
-        var $el = $(this);
-        var match = bugRe.exec($el.text());
+        let match = bugRe.exec(this.textContent);
         if (match) {
-            bugIds.push(match[1]);
+            let bugId = match[1];
+            if (bugIds.indexOf(bugId) === -1) {
+                bugIds.push(bugId);
+            }
         }
     });
 
-    // uniquify bugs
-    // via http://paulirish.com/2010/duck-punching-with-jquery/
-    bugIds = $.grep(bugIds, function(v, k){
-        return $.inArray(v, bugIds) === k;
-    });
+    return bugIds;
+}
 
-    return $.map(bugIds, function(k){
+
+function getBugLinks(){
+    let bugIds = getBugIds();
+
+    return bugIds.map(function(k){
         return '<a href="' + bugURL + k + '" target="_blank" class="bugzilla-link">' + k + '</a>';
     });
 }
 
 
 var $bugsList = $([
-    '<p class="explain" id="bugzilla-links">Bugs in commits: ',
-    getBugLinks().join(', '),
-    '</p>'
-].join('')).insertBefore('#commits_bucket .boxed-group:first');
+        '<p class="explain" id="bugzilla-links">Bugs in commits: ',
+        getBugLinks().join(', '),
+        '</p>'
+    ].join('')).insertBefore('#commits_bucket .boxed-group:first');
 
 $('<p class="explain"><a href="#">Open all in tabs</a></p>')
     .insertAfter($bugsList)
     .find('a').on('click', function(e){
         e.preventDefault();
-        $('#bugzilla-links a').each(function(){
-            window.open($(this).attr('href'));
+        $bugsList.find('a').each(function(){
+            window.open(this.href);
         });
     });
